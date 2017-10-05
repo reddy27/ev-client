cd87d1107b87b24c8a8c837875faeacfccec7a0a
reddy27 <gaddamdev5@gmail.com>
2017-10-04 20:23:13 -0700

    setted port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-
+app.set('port', (process.env.PORT || 5000));
 //app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 //var server = app.listen(4005);
